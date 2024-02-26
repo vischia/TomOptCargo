b75b75a24aa03545c7ddda38de7288e4a4ee5d6d
SamuelAL185 <samuelalvarez185@gmail.com>
2024-02-26 20:45:21 +0100

    added the panel type selection

--- hodoscope.py
+++ hodoscope.py
@@ -1,9 +1,9 @@
-from typing import Tuple, List, Optional
+from typing import Tuple, List, Optional, Union
 from tomopt.core import DEVICE
 import torch
 from torch import Tensor
 from torch import nn
-from tomopt.volume.panel import DetectorPanel
+from tomopt.volume.panel import DetectorPanel, SigmoidDetectorPanel
 
 
 class Hodoscope(nn.Module):
@@ -19,6 +19,8 @@ class Hodoscope(nn.Module):
                  m2_cost: float = 1.,
                  budget: Optional[Tensor] = None,
                  realistic_validation: bool = True,
+                 panel_type: str = 'SigmoidDetectorPanel',
+                 smooth: Union[float, Tensor] = None,
                  device: torch.device = DEVICE):
         
         super().__init__()
@@ -31,31 +33,48 @@ class Hodoscope(nn.Module):
         self.n_panels = n_panels
         self.res = res
         self.eff = eff
+        self.panel_type = panel_type
+        self.smooth = smooth
         self.panels = self.generate_init_panels()
-
         self.device = device
+
     
     def __getitem__(self, idx: int) -> DetectorPanel:
         return self.panels[idx]
 
-    def generate_init_panels(self) -> List[DetectorPanel]:
+    def generate_init_panels(self) -> Union[List[DetectorPanel],List[SigmoidDetectorPanel]]:
 
         r"""
         Generates Detector panels based on the xy and z position (xy, z), the span of the hodoscope (xyz_span), 
         and the gap between the edge of the hodoscope and the panels (xyz_gap).
 
         Returns:
-            DetectorPanles as a nn.ModuleList.
+            DetectorPanels as a nn.ModuleList.
         """
         
-        return nn.ModuleList(
-            [DetectorPanel(res=self.res, 
-                           eff=self.eff,
-                           init_xyz=[self.xy[0],
-                                     self.xy[1],
-                                     self.z - self.xyz_gap[2] - (self.xyz_span[2]-2*self.xyz_gap[2])*i/(self.n_panels-1)], 
-                           init_xy_span=[self.xyz_span[0] - 2 * self.xyz_gap[0], self.xyz_span[1] - 2 * self.xyz_gap[1]],
-                           device=DEVICE) for i in range(self.n_panels)])
+        if self.panel_type == 'DetectorPanel':
+            return nn.ModuleList(
+                [DetectorPanel(res = self.res, 
+                            eff = self.eff,
+                            init_xyz = [self.xy[0],
+                                        self.xy[1],
+                                        self.z - self.xyz_gap[2] - (self.xyz_span[2]-2*self.xyz_gap[2])*i/(self.n_panels-1)], 
+                            init_xy_span = [self.xyz_span[0] - 2 * self.xyz_gap[0], self.xyz_span[1] - 2 * self.xyz_gap[1]],
+                            device=DEVICE) for i in range(self.n_panels)])
+        
+        if self.panel_type == 'SigmoidDetectorPanel':
+            return nn.ModuleList(
+                [SigmoidDetectorPanel(smooth = self.smooth,
+                            res = self.res, 
+                            eff = self.eff,
+                            init_xyz = [self.xy[0],
+                                        self.xy[1],
+                                        self.z - self.xyz_gap[2] - (self.xyz_span[2]-2*self.xyz_gap[2])*i/(self.n_panels-1)], 
+                            init_xy_span = [self.xyz_span[0] - 2 * self.xyz_gap[0], self.xyz_span[1] - 2 * self.xyz_gap[1]],
+                            device=DEVICE) for i in range(self.n_panels)])
+        
+        else:
+            raise ValueError(f"Detector type {self.panel_type} currently not supported.")
 
     def get_cost(self) -> Tensor:
 
