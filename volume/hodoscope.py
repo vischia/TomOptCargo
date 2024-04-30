15e71f45c679085eb8697957170c768ea1025dcd
GEANT <maximelagrange98@gmail.com>
2024-04-30 16:28:01 +0200

    added documentation

--- volume/hodoscope.py
+++ volume/hodoscope.py
@@ -1,14 +1,37 @@
 from typing import Tuple, List, Optional, Union
-from tomopt.core import DEVICE
 import numpy as np
 import torch
 from torch import Tensor, nn
 
 from volume.panel import HodoscopeDetectorPanel
+
+from tomopt.core import DEVICE
+
+r"""
+Provides implementations of class simulating hodoscope-style detectors with learnable xyz positions.
+"""
+
 __all__ = ["Hodoscope"]
 
 
 class Hodoscope(nn.Module):
+    r"""
+    Provides a Hodoscope detection system containing indepedent detction panels, 
+    centered at a learnable parameter `xyz` position and with a fixed span `xy_span`.
+    Whilst this class can be used manually, it is designed to be used by the :class:`~tomopt.volume.layer.PanelDetectorLayer` class.
+    
+    Once istanciated, the Hodoscope class creates HodoscopedetectorPanels centered at `xy`. They are responsible for the muon hit recording throught their `get_hits` method.
+    
+    Arguments:
+        init_xyz: initial xyz position of the hodoscope in metres in the volume frame. The z position corresponds to the top of the hodoscope.
+        init_xyz_span: initial xyz-span (total width) of the hodoscope in metres.
+        xyz_gap: the gap between the edges of the hodoscope and the panels.
+        n_panels: the number of detection panels within the hodoscope.
+        res: resolution of the panels in m^-1, i.e. a higher value improves the precision on the hit recording.
+        eff: efficiency of the hit recording of the panel, indicated as a probability [0,1].
+        realistic_validation: if True, will use the physical interpretation of the panel during evaluation.
+        device: device on which to place tensors.
+    """
 
     def __init__(self,
                  *,
@@ -18,11 +41,7 @@ class Hodoscope(nn.Module):
                  n_panels: int = 3,
                  res: float = 1000,
                  eff: float = 0.9,
-                 m2_cost: float = 1.,
-                 budget: Optional[Tensor] = None,
                  realistic_validation: bool = False,
-                 panel_type: str = 'HodoscopeDetectorPanel',
-                 smooth: Union[float, Tensor] = None,
                  device: torch.device = DEVICE):
         
         if res <= 0:
@@ -35,13 +54,10 @@ class Hodoscope(nn.Module):
         self.xy = nn.Parameter(torch.tensor(init_xyz[:2], device=self.device))
         self.z = nn.Parameter(torch.tensor(init_xyz[2:3], device=self.device))
         self.xyz_span = torch.Tensor(init_xyz_span, device=self.device)
-        self.register_buffer("m2_cost", torch.tensor(float(m2_cost), device=self.device))
         self.register_buffer("resolution", torch.tensor(float(res), device=self.device))
         self.register_buffer("efficiency", torch.tensor(float(eff), device=self.device))
         self.xyz_gap = xyz_gap
         self.n_panels = n_panels
-        self.panel_type = panel_type
-        self.smooth = smooth
         self.panels = self.generate_init_panels()
         self.device = device
 
@@ -49,8 +65,7 @@ class Hodoscope(nn.Module):
     def __getitem__(self, idx: int) -> HodoscopeDetectorPanel:
         return self.panels[idx]
 
-    def generate_init_panels(self) -> Union[List[HodoscopeDetectorPanel]]:
-
+    def generate_init_panels(self) -> List[HodoscopeDetectorPanel]:
         r"""
         Generates Detector panels based on the xy and z position (xy, z), the span of the hodoscope (xyz_span), 
         and the gap between the edge of the hodoscope and the panels (xyz_gap).
@@ -58,20 +73,15 @@ class Hodoscope(nn.Module):
         Returns:
             DetectorPanels as a nn.ModuleList.
         """
-        
-        if self.panel_type == 'HodoscopeDetectorPanel':
-            return [HodoscopeDetectorPanel(realistic_validation = self.realistic_validation,
-                                           idx = i, 
-                            init_xy_span = [self.xyz_span[0] - 2 * self.xyz_gap[0], self.xyz_span[1] - 2 * self.xyz_gap[1]],
-                            device=DEVICE, 
-                            hod = self) for i in range(self.n_panels)]
-        else:
-            raise ValueError(f"Detector type {self.panel_type} currently not supported.")
-        
+        return [HodoscopeDetectorPanel(realistic_validation = self.realistic_validation,
+                                        idx = i, 
+                                        init_xy_span = [self.xyz_span[0] - 2 * self.xyz_gap[0], self.xyz_span[1] - 2 * self.xyz_gap[1]],
+                                        device = DEVICE, 
+                                        hod = self) for i in range(self.n_panels)]
+
     def clamp_params(self, xyz_low: Tuple[float, float, float], xyz_high: Tuple[float, float, float]) -> None:
         r"""
-        Ensures that the panel is centred within the supplied xyz range,
-        and that the span of the panel is between xyz_high/20 and xyz_high*10.
+        Ensures that the panel is centred within the supplied xyz range.
         A small random number < 1e-3 is added/subtracted to the min/max z position of the panel, to ensure it doesn't overlap with other panels.
 
         Arguments:
@@ -84,17 +94,19 @@ class Hodoscope(nn.Module):
             self.xy[0].clamp_(min=xyz_low[0], max=xyz_high[0])
             self.xy[1].clamp_(min=xyz_low[1], max=xyz_high[1])
             self.z.clamp_(min=xyz_low[2] + eps, max=xyz_high[2] - eps)
-            self.xyz_span[0].clamp_(min=xyz_high[0] / 20, max=10 * xyz_high[0])
-            self.xyz_span[1].clamp_(min=xyz_high[1] / 20, max=10 * xyz_high[1])
 
-    def get_xyz_min(self) -> Tuple[float, float, float]:
+    def xyz_min(self) -> Tensor:
+        r"""
+        Returns the front lower left corner position of the hodoscope in meters.
+        """
+        return torch.tensor([self.xy[0].item()-self.xyz_span[0].item() / 2, 
+                             self.xy[1].item()-self.xyz_span[1].item() / 2, 
+                             self.z.item()-self.xyz_span[2].item()])
 
-        return [self.xy[0].item()-self.xyz_span[0].item(), self.xy[1].item()-self.xyz_span[1].item(), self.z.item()-self.xyz_span[2].item()]
-
-    def get_xyz_max(self) -> Tuple[float, float, float]:
-
-        return [self.xy[0].item()+self.xyz_span[0].item(), self.xy[1].item()+self.xyz_span[1].item(), self.z.item()+self.xyz_span[2].item()]
-
-    def get_cost(self) -> Tensor:
-
-        return torch.sum(torch.Tensor([p.get_cost() for p in self.panels]))
+    def xyz_max(self) -> Tensor:
+        r"""
+        Returns the back upper right corner position of the hodoscope in meters.
+        """
+        return torch.tensor([self.xy[0].item() + self.xyz_span[0].item() / 2, 
+                             self.xy[1].item() + self.xyz_span[1].item() / 2, 
+                             self.z.item()])
